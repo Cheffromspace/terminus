46c54497c6e56407fb9b1ba94f9cb22bc99b520a
Cheffromspace <jonflatt@gmail.com>
2025-02-16 17:05:20 -0600

    chore: prepare package for npm publishing

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
 // Components
-export { default as SearchBar } from './components/SearchBar';
 export { default as MarkdownRenderer } from './components/MarkdownRenderer';
-export { default as PostUpdater } from './components/PostUpdater';
-export { default as NavigationButtons } from './components/NavigationButtons';
+export { ThemeProvider } from './components/ThemeProvider';
+export { default as KeyboardNavigation } from './components/KeyboardNavigation';
 
-// Utils
-export { 
-  getStaticPosts,
-  getStaticPostBySlug,
-  getStaticPreviousAndNextPosts,
-  getFeaturedStaticPosts
-} from './utils/server-posts';
+// Hooks
+export { useTheme } from './hooks/useTheme';
+
+// Themes
+export * from './themes';
 
 // Types
-export type { Post, Series } from './types/blog';
+export * from './types/theme';
+export * from './types/blog';
 
-// Contexts
+// Context
 export { PostContext, PostProvider } from './contexts/PostContext';
